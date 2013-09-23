7a8d1ab2ddcd449dc8a76fec22bee55acce8e7b0
James Cunningham <james@einherj.ar.com>
2013-09-23 19:45:37 +0000

    Updated callback location.

--- public/js/seevolution.js
+++ public/js/seevolution.js
@@ -1,8 +1,8 @@
 
 CloudFlare.define(
-    '<appKey>',
-    ['<appKey>/config'],
+    'seevolution',
+    [],
     function(config) {
-        CloudFlare.require(['https://www.seevolution.com/cjs']);
+        CloudFlare.require(['https://www.seevolution.com/collector/v2/build/js/svlu.js']);
     }
 );
